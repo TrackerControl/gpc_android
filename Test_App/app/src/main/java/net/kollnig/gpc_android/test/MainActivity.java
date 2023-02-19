5de829b281f82296901489ee42412c69418cf324
Nishant <naggarwal@wesleyan.edu>
2023-02-19 16:34:19 -0500

    Deleted deprecated code, startActivityForResult, and switched it for ActivityResultLauncher

--- Test_App/app/src/main/java/net/kollnig/gpc_android/test/MainActivity.java
+++ Test_App/app/src/main/java/net/kollnig/gpc_android/test/MainActivity.java
@@ -1,5 +1,7 @@
 package net.kollnig.gpc_android.test;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -11,8 +13,10 @@ import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_TEST = 10;
+    private static final String EXTRA_REQUEST_CODE = "net.kollnig.gpc_android.test.REQUEST_CODE";
 
     String nonce = null;
+    ActivityResultLauncher<Intent> launcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,25 +24,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         nonce = UUID.randomUUID().toString();
+        launcher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        int requestCode = data.getIntExtra(EXTRA_REQUEST_CODE, 0);
+                        if (requestCode == REQUEST_TEST) {
+                            String nonce = data.getStringExtra("nonce");
+
+                            if (data.hasExtra("gpc") && this.nonce != null && this.nonce.equals(nonce)) {
+                                boolean gpc = data.getBooleanExtra("gpc", false);
+                                ((TextView) findViewById(R.id.lbl_hello)).setText(String.format("GPC: %s", gpc));
+                            }
+                        }
+                    }
+                }
+        );
         Intent myIntent = new Intent();
         myIntent.setClassName("net.kollnig.gpc_android", "net.kollnig.gpc_android.ConsentActivity");
+        myIntent.putExtra(EXTRA_REQUEST_CODE, REQUEST_TEST);
         myIntent.putExtra("nonce", nonce);
-        startActivityForResult(myIntent, REQUEST_TEST);
+        launcher.launch(myIntent);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == REQUEST_TEST) {
-            if(resultCode == Activity.RESULT_OK) {
-                String nonce = data.getStringExtra("nonce");
-
-                if (data.hasExtra("gpc") && this.nonce != null && this.nonce.equals(nonce)) {
-                    boolean gpc = data.getBooleanExtra("gpc", false);
-                    ((TextView) findViewById(R.id.lbl_hello)).setText(String.format("GPC: %s", gpc));
-                }
-            }
-        }
-    }
 }
